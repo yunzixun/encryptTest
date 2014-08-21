63ba325ca047d503be04319b6c4fbd37e3ec516b
lionjohn <lingzhao@qq.com>
2014-08-21 21:01:47 +0800

    first commit

--- encryptTest/FSAppDelegate.m
+++ encryptTest/FSAppDelegate.m
@@ -7,12 +7,33 @@
 //
 
 #import "FSAppDelegate.h"
+#import "EncryptedFileURLProtocol.h"
+#import "NSURL+EncryptedFileURLProtocol.h"
+
+@interface FSAppDelegate ()
+@property (weak) IBOutlet NSImageView *testImageV;
+
+@end
 
 @implementation FSAppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
     // Insert code here to initialize your application
+    
+    [NSURLProtocol registerClass:[EncryptedFileURLProtocol class]];
+    
+    
+    NSString *indexPath = [[NSBundle mainBundle] pathForResource:@"link_b03_01" ofType:@"png" inDirectory:nil];
+    
+    NSURL *url = [NSURL encryptedFileURLWithPath:indexPath];
+    NSData *data = [NSData dataWithContentsOfURL:url];
+    
+    NSImage *image = [[NSImage alloc] initWithData:data];
+    
+    _testImageV.image = image;
+    
+    
 }
 
 @end
